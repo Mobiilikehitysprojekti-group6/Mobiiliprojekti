9082e4234b49a74f67cd96f4ca0341b567c385b5
j3shi <jebuyt@gmail.com>
2026-01-22 13:18:01 +0200

    added global color scheme file

---
+++ src/components/colors.ts
@@ -0,0 +1,13 @@
+export const lightColors = {
+    background: '#FEFEFA',
+    text: '#111111',
+    secondaryText: '#555555',
+    accent: '#7ed957',
+}
+
+export const darkColors = {
+    background: "#242124",
+    text: '#FEFEFA',
+    secondaryText: '#AAAAAA',
+    accent: '#7ed957',
+}
